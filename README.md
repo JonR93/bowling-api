a80d6f469f1d81bfe30d5bf9a9fc88d9e0da68b9
Jonathan <jon.ruel93@gmail.com>
2022-05-08 15:56:11 -0400

    Initial Commit after initializing a new SpringBoot project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bowling-api
+
 A web service server that responds to specific API calls that will simulate a bowling game server
---
+++ src/test/java/com/springboot/bowling/BowlingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bowling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
